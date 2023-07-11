383773579be1560cc295947518b1e99a0f2d1cac
tataprin <tataprin1234@gmail.com>
2023-07-11 16:52:18 +0700

    vat cal
    add vat cal + index

---
+++ Vat_Cal.py
@@ -0,0 +1,8 @@
+price = int(input("Enter Your Price"))
+print(price + (price * 7/100))
+
+price = int(input("Enter Your Price"))
+vat = 7/100
+print(price + (price * vat))
+
+
--- index.py
+++ index.py
@@ -1,1 +1,46 @@
-print("Hello")
+
+'''print("hello")
+print("132123")
+print(1)
+print(1.2)
+print(7j + 8j)
+print(True)
+print(False)
+'''
+''' Data Types'''
+'''
+print(type("hello"))
+print(type(1))
+print(type(1.22))
+print(type(True))
+'''
+
+'''
+print(1+1)
+print(1.2+1)
+print("hello" + "tata")
+
+
+
+print(2!=2)
+
+x = 6
+y = "hello"
+
+
+x = int(input())
+print(type(x))
+'''
+'''calculator'''
+
+x = int(input("first number"))
+y = int(input("second number"))
+print(x+y)
+
+#input name lastname
+
+name = input("Enter your Name")
+last = input("Enter your LastName")
+
+print(name, last)
+print(name + last)
