c955ed91536a52f4934c056db0acf66df11ea9ce
tataprin <tataprin1234@gmail.com>
2023-07-18 16:27:42 +0700

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -33,8 +33,8 @@
 # while True:
 #     print("hello")
 
-user = input("Enter Your Username ")
-passw = input("Enter Your Password ")
+# user = input("Enter Your Username ")
+# passw = input("Enter Your Password ")
 
 
 # while user != "admin" and passw != 1234:
@@ -43,13 +43,56 @@ passw = input("Enter Your Password ")
 #     passw = input("Enter Your Password ")
 # print("done")
 
-x = 0
-while x == 0:
-    user = input("Enter Your Username ")
-    passw = input("Enter Your Password ")
-    if user == "admin" and passw == "1234":
-        print("done")
-        x = 1
-    else:
-        print("wrong")
+
+# while True:
+#     user = input("Enter Your Username ")
+#     passw = input("Enter Your Password ")
+#     if user == "admin" and passw == "1234":
+#         print("done")
+#         break
+#     else:
+#         print("wrong")
         
+
+# print(list(range(10)))
+
+# for x in range(10):
+#     print("hello", x)
+
+# num = 2
+# for x in range(12):
+#     print(num, "x", x+1, "=", num*(x+1))
+
+# money = 500
+# if money >= 300:
+#     print("KFC!!!")
+#     if money == 300:
+#         money -= 300
+# elif money == 50:
+#     print("wingsab")
+# else:
+#     print("Broke")
+
+# user = input("enter your username")
+# passw = input("enter your password")
+
+# if user == "admin" and passw == "1234":
+#     print("done")
+# else:
+#     print("False")
+# x = 0
+# while x < 5:
+#     print("hello",x)
+#     x += 1
+# num = int(input("enter number"))
+
+# for x in range(12):
+#     print(num,"x", x, "=", num*(x))
+
+# x = 10
+# print(list(range(x)))
+
+for x in range(12):
+    for y in range(12):
+        print(x+1, "x", y+1, "=", (x+1)*(y+1))
+
